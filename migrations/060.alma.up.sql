471629121a01b1c05a6417fa3318b33b762aa534
Alma <alma.wettig@posteo.com>
2024-07-02 14:08:11 +0200

    corrected the type of the data

--- migrations/060.alma.up.sql
+++ migrations/060.alma.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE buildings ADD COLUMN IF NOT EXISTS number_persons smallint; -- Number of people in this household
-ALTER TABLE buildings ADD COLUMN IF NOT EXISTS reference_period year; -- Referenz Period (Year)
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS reference_period smallint; -- Referenz Period (Year)
 ALTER TABLE buildings ADD COLUMN IF NOT EXISTS electricity_usage float; -- in kWh
 ALTER TABLE buildings ADD COLUMN IF NOT EXISTS gas_usage float; -- in kWh
 ALTER TABLE buildings ADD COLUMN IF NOT EXISTS living_area float; -- Size of the appartment/ the house in m^2
